b8c42a0bef5373d326af7e05361b44a3b5212f03
Sakthivelaayyasamy <sakthivel.ayyasamy@kryasolutions.com>
2025-04-08 16:32:00 +0530

    theme css
    theme css updated

--- src/app/easy-view/easy-view.component.ts
+++ src/app/easy-view/easy-view.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import { RegistrationComponent } from './registration/registration.component';
-import { SourcingEventsComponent } from "./sourcing-events/sourcing-events.component";
-import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
-import { ContractComponent } from './contract/contract.component';
-import { InvoiceStatusComponent } from './invoice-status/invoice-status.component';
-import { CollaborationListComponent } from './collaboration-list/collaboration-list.component';
 @Component({
   selector: 'app-easy-view',
   standalone: true,
-  imports: [MatTabsModule, RegistrationComponent, SourcingEventsComponent, PurchaseOrderComponent, ContractComponent, InvoiceStatusComponent, CollaborationListComponent],
+  imports: [],
   templateUrl: './easy-view.component.html',
   styleUrl: './easy-view.component.scss'
 })
