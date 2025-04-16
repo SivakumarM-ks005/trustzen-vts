ed1ec7d8aeb643b7533f25a2b9f234fc8a5e869d
Sakthivelaayyasamy <sakthivel.ayyasamy@kryasolutions.com>
2025-04-16 10:23:14 +0530

    Bank details ts removed and updated
    Bank details ts removed and updated

--- src/app/supplier-user-form/bank-details/bank-details.component.ts
+++ src/app/supplier-user-form/bank-details/bank-details.component.ts
@@ -1,9 +1,8 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonService } from '../../core/services/common.service';
 import { AdminService } from '../../core/services/admin/admin.service';
 import { MatDialog } from '@angular/material/dialog';
-import { ConfirmationDialogComponent } from '../../confirmation-dialog/confirmation-dialog.component';
 import { ActivatedRoute } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
 import { MatButton } from '@angular/material/button';
@@ -12,10 +11,9 @@ import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatSelect } from '@angular/material/select';
 import { MatOption } from '@angular/material/core';
-import { MatCheckbox, MatCheckboxChange } from '@angular/material/checkbox';
+import { MatCheckbox } from '@angular/material/checkbox';
 import { LoginService } from '../../core/services/login/login.service';
 import { SupplierUserFormService } from '../../core/services/supplier-management/supplier.user.form.service';
-import { PreQualificationProcessComponent } from '@app/pre-qualification-process/pre-qualification-process.component';
 
 @Component({
   selector: 'app-bank-details',
@@ -36,23 +34,6 @@ export class BankDetailsComponent implements OnInit {
   @Output() dialogResult = new EventEmitter<boolean>();
   @Output() nextTabEmit = new EventEmitter();
   @Output() tabValidCheckEmit = new EventEmitter();
-  defaultBank: boolean = false;
-  disableStatusBased: boolean = true;
-  AccNoMinchar: any;
-  AccNomaxChar: any;
-  IbanminChar: any;
-  IbanmaxChar: any;
-  SwiftminChar: any;
-  SwiftmaxChar: any;
-  BsbminChar: any;
-  BsbmaxChar: any;
-  RouteminChar: any;
-  RoutemaxChar: any;
-  AccManantory: any;
-  IbanManantory: any;
-  SwiftManantory: any;
-  BsbManantory: any;
-  RouteManantory: any;
   isSubmitted = false;
   @Output() NextFlag = new EventEmitter<boolean>();
   profileStatus: any;
@@ -79,7 +60,6 @@ export class BankDetailsComponent implements OnInit {
 
       if (response.status === 'In-Progress') {
         this.BankForm.disable();
-        this.disableStatusBased = false;
       } else if (response.profile === 'manageprofile') {
         this.profileStatus = response.profile;
       }
@@ -91,21 +71,6 @@ export class BankDetailsComponent implements OnInit {
       if (res) {
         console.log(res.complianceRenewalBank.accManantory);
 
-        this.AccNoMinchar = res.complianceRenewalBank.accNominChar || 0;
-        this.AccNomaxChar = res.complianceRenewalBank.accNomaxChar || 0;
-        this.AccManantory = res.complianceRenewalBank.accManantory || false;
-        this.IbanManantory = res.complianceRenewalBank.ibanManantory || false;
-        this.IbanminChar = res.complianceRenewalBank.ibanminChar || 0;
-        this.IbanmaxChar = res.complianceRenewalBank.ibanmaxChar || null;
-        this.SwiftManantory = res.complianceRenewalBank.swiftManantory || false;
-        this.SwiftminChar = res.complianceRenewalBank.swiftminChar || 0;
-        this.SwiftmaxChar = res.complianceRenewalBank.swiftmaxChar || null;
-        this.BsbManantory = res.complianceRenewalBank.bsbManantory || false;
-        this.BsbminChar = res.complianceRenewalBank.bsbminChar || 0;
-        this.BsbmaxChar = res.complianceRenewalBank.bsbmaxChar || null;
-        this.RouteManantory = res.complianceRenewalBank.routeManantory || false;
-        this.RouteminChar = res.complianceRenewalBank.routeminChar || 0;
-        this.RoutemaxChar = res.complianceRenewalBank.routemaxChar || null;
         this.initForm(bank);
       }
     })
@@ -130,43 +95,29 @@ export class BankDetailsComponent implements OnInit {
 
         bank?.accountNumber || '',
         [
-          ...(this.AccManantory ? [Validators.required] : []),
-          ...(this.AccManantory ? [Validators.minLength(this.AccNoMinchar)] : []),
-          ...(this.AccManantory ? [Validators.maxLength(this.AccNomaxChar)] : []),
         ],
       ],
 
       swiftCodeBIC: [
         bank?.swiftCodeBIC || '',
         [
-          ...(this.SwiftManantory ? [Validators.required] : []),
-          ...(this.SwiftManantory ? [Validators.minLength(this.SwiftminChar)] : []),
-          ...(this.SwiftManantory ? [Validators.maxLength(this.SwiftmaxChar)] : []),
         ],
       ],
       iban: [
         bank?.iban || '',
         [
-          ...(this.IbanManantory ? [Validators.required] : []),
-          ...(this.IbanManantory ? [Validators.minLength(this.IbanminChar)] : []),
-          ...(this.IbanManantory ? [Validators.maxLength(this.IbanmaxChar)] : []),
+
         ],
       ],
 
       bsb: [
         bank?.bsb || null,
         [
-          ...(this.BsbManantory ? [Validators.required] : []),
-          ...(this.BsbManantory ? [Validators.minLength(this.BsbminChar)] : []),
-          ...(this.BsbManantory ? [Validators.maxLength(this.BsbmaxChar)] : []),
         ],
       ],
       routing: [
         bank?.routing || null,
         [
-          ...(this.RouteManantory ? [Validators.required] : []),
-          ...(this.RouteManantory ? [Validators.minLength(this.RouteminChar)] : []),
-          ...(this.RouteManantory ? [Validators.maxLength(this.RoutemaxChar)] : []),
         ],
       ],
       ifscCode: [bank?.ifscCode || null],
@@ -174,7 +125,6 @@ export class BankDetailsComponent implements OnInit {
       defaultBank: [bank?.defaultBank || false],
     });
 
-    this.checkCorresBank(bank?.correspondentBankObject);
   }
 
   updateDefaultBankStatus() {
@@ -215,193 +165,6 @@ export class BankDetailsComponent implements OnInit {
     });
   }
 
-  preQualification(): void {
-
-    const cancelDialogRef = this.dialog.open(PreQualificationProcessComponent, {
-      disableClose: false,
-      hasBackdrop: true,
-      autoFocus: true,
-      width: '35%',
-      height: '40%',
-      position: {
-        top: 'calc(10vw + 20px)',
-      },
-      panelClass: 'confirmdialog',
-      data: {
-        moduleName: 'Supplier Bank'
-      },
-    });
-
-  }
-
-  ClearValues(): boolean {
-    // this.SaveDraftFlag.emit(this.commonService.CommonClearValues(this.BankForm));
-    if (this.BankForm.valid) {
-      this.SaveDraftFlag.emit(true)
-    } else {
-      this.SaveDraftFlag.emit(false)
-    }
-    return this.commonService.CommonClearValues(this.BankForm);
-  }
-
-
-
-
-
-  EditBank(data: any, i: number, el: HTMLElement) {
-    el.scrollIntoView();
-    this.editFlag = true;
-    this.getStates(data.countryId);
-    this.getCities(data.stateId);
-    this.initForm(data)
-    this.activateRouter?.params?.subscribe((response) => {
-
-      if (response.status === 'In-Progress') {
-        this.BankForm.disable();
-        this.disableStatusBased = false;
-      }
-
-    });
-  }
-
-
-
-
-
-  resetForm() {
-    this.BankForm.removeControl('correspondentBankObject');
-    this.BankForm.reset();
-    this.editFlag = false;
-  }
-
-  allowOnlyNumbers(event: KeyboardEvent): void {
-    const charCode = event.key.charCodeAt(0);
-    // Allow only numeric keys (0-9)
-    if (charCode < 48 || charCode > 57) {
-      event.preventDefault();
-    }
-  }
-  deletePopUp(ind: number) {
-    const cancelDialogRef = this.dialog.open(ConfirmationDialogComponent, this.commonService.deletetModalConfig);
-    cancelDialogRef.afterClosed().subscribe(result => {
-      if (result) {
-
-      } else {
-        this.dialog.closeAll();
-      }
-    });
-  }
-
-  enableBankButton() {
-    if (this.editFlag == false) {
-      const bankFields = Object.entries(this.BankForm.controls)
-        .filter(([key]) => key !== 'defaultBank')
-        .some(([_, control]) => control.value);
-      const defaultBankControl = this.BankForm.get('defaultBank');
-
-      if (bankFields) {
-
-      } else {
-
-      }
-    }
-  }
-
-  checkCorBan(event: MatCheckboxChange): void {
-    if (event?.checked) this.checkCorresBank(); else this.BankForm.removeControl('correspondentBankObject');
-  }
-
-  checkCorresBank(bank?: any) {
-    if (this.BankForm.controls['correspondentBank'].value) {
-      this.getCountryForCorrespondanceBank();
-      this.getCurrencyForCorrespondanceBank();
-      this.BankForm.addControl('correspondentBankObject', this.fb.group({
-        bankId: [bank?.bankId || 0],
-        supplierId: [bank?.supplierId || 0],
-        loggedIn: [bank?.loggedIn || 0],
-        bankName: [bank?.bankName || '', Validators.required],
-        branch: [bank?.branch || '', Validators.required],
-        currencyId: [bank?.currencyId || null, Validators.required],
-        countryId: [bank?.countryId || null, Validators.required],
-        stateId: [bank?.stateId || null, Validators.required],
-        cityId: [bank?.cityId || null, Validators.required],
-        currencyName: [bank?.currency || ''],
-        cityName: [bank?.city || ''],
-        stateName: [bank?.state || ''],
-        countryName: [bank?.country || ''],
-
-        accountNumber: [
-          bank?.accountNumber || '',
-          [
-            ...(this.AccManantory ? [Validators.minLength(this.AccNoMinchar)] : []),
-            ...(this.AccManantory ? [Validators.maxLength(this.AccNomaxChar)] : []),
-            ...(this.AccManantory ? [Validators.required] : []),
-          ],
-        ],
-
-        swiftCodeBIC: [
-          bank?.swiftCodeBIC || '',
-          [
-            ...(this.SwiftManantory ? [Validators.required] : []),
-            ...(this.SwiftminChar ? [Validators.minLength(this.SwiftminChar)] : []),
-            ...(this.SwiftmaxChar ? [Validators.maxLength(this.SwiftmaxChar)] : []),
-          ],
-        ],
-        iban: [
-          bank?.iban || '',
-          [
-            ...(this.IbanManantory ? [Validators.required] : []),
-            ...(this.IbanminChar ? [Validators.minLength(this.IbanminChar)] : []),
-            ...(this.IbanmaxChar ? [Validators.maxLength(this.IbanmaxChar)] : []),
-          ],
-        ],
-
-        bsb: [
-          bank?.bsb || null,
-          [
-            ...(this.BsbManantory ? [Validators.required] : []),
-            ...(this.BsbminChar ? [Validators.minLength(this.BsbminChar)] : []),
-            ...(this.BsbmaxChar ? [Validators.maxLength(this.BsbmaxChar)] : []),
-          ],
-        ],
-        routing: [
-          bank?.routing || null,
-          [
-            ...(this.RouteManantory ? [Validators.required] : []),
-            ...(this.RouteminChar ? [Validators.minLength(this.RouteminChar)] : []),
-            ...(this.RoutemaxChar ? [Validators.maxLength(this.RoutemaxChar)] : []),
-          ],
-        ],
-        ifscCode: [bank?.ifscCode || null],
-      }))
-    } else {
-      this.BankForm.removeControl('correspondentBankObject');
-    }
-  }
-
-  getCountryForCorrespondanceBank(): void {
-    this.loginservice.getCountry().subscribe(data => {
-      if (data) {
-      }
-    });
-  }
-
-  getCurrencyForCorrespondanceBank(): void {
-    this.loginservice.GetCurrencyDetails().subscribe(data => {
-      if (data) {
-      }
-    });
-  }
-
-  getStatesForCorrespondanceBank(countryId: number): void {
-    this.loginservice.GetCountryBaseaState(countryId).subscribe((data) => {
-    });
-  }
-
-  getCitiesForCorrespondanceBank(stateId: number): void {
-    this.loginservice.GetStatebaseCity(stateId).subscribe((data) => {
-    });
-  }
 }
 
 
